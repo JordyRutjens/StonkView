e38b28b98869770f2659e1722445d21edafeec98
Jordy Rutjens <jordyrutjenspp@gmail.com>
2020-12-18 20:52:00 +0100

    Oplevering - 18-12-2020

---
+++ StonkView.DataAccess/FavoriteDAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using StonkView.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace StonkView.DataAccess
+{
+    public class FavoriteDAL
+    {
+       
+    }
+}
